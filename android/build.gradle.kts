41a7c8c1e30862024bc8f0f3a40c28d597b58a22
rahuljose <prahuljose@gmail.com>
2026-05-26 23:28:23 +0530

    commit

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -16,6 +16,7 @@ subprojects {
     project.layout.buildDirectory.value(newSubprojectBuildDir)
     
     // Fix for Namespace not specified in older packages
+    // Fix compileSdk for plugins that use an outdated API level (e.g. flutter_tflite lStar error)
     afterEvaluate {
         if (project.hasProperty("android")) {
             val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
@@ -24,6 +25,10 @@ subprojects {
                 if (android.namespace.isNullOrEmpty()) {
                     android.namespace = "com.example.${project.name.replace("-", "_")}"
                 }
+            }
+            val current = android.compileSdkVersion
+            if (current == null || current < "android-34") {
+                android.compileSdkVersion(34)
             }
         }
     }
